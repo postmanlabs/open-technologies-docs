e072b4e007a263fc1aec556ee2c2a93155447465
OlliesWorld <rtlockwoodwork@gmail.com>
2023-01-19 08:03:15 -0700

    hide the development if statement

--- src/components/ContextualLinks/ContextualLinks.jsx
+++ src/components/ContextualLinks/ContextualLinks.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { v4 as uuidv4 } from "uuid";
+const { v4: uuidv4 } = require('uuid');
 import DynamicLink from '../Shared/DynamicLink';
 import recentBlogPosts from '../../../bff-data/blog.json';
 import styled from "styled-components";
@@ -69,7 +69,7 @@ const ContextualLinks = ({ links }) => (
       links.map((item) => {
         if (item.type === 'link') {
           return (
-            <div className="contextual-links__link"  key={uuidv4()}>
+            <div className="contextual-links__link" key={uuidv4()}>
               <DynamicLink url={item.url} name={item.name} />
             </div>
           );
@@ -81,21 +81,21 @@ const ContextualLinks = ({ links }) => (
             </div>
           );
         }
-        if (item.type === 'dynamic_blog' && item.blog_tag) {
-          if (Object.keys(recentBlogPosts).length === 0) {
-            // If recentBlogPosts.length === 0, then either there is no .env.development,
-            // it has a bad blog url, or the endpoint returned something bad
-            return (
-              <div className="contextual-links__alert" role="alert" key={uuidv4()}>
-                <p>
-                  You are currently in develop mode.
-                  Dynamic blog posts will not be displayed locally.
-                  <a style={{ fontSize: 'inherit' }} href="https://github.com/postmanlabs/postman-docs/blob/develop/CONTRIBUTING.md#to-use-the-dynamic-blog-posts-feature" target="_blank" rel="noopener">See Contributing doc for details</a>
-                  .
-                </p>
-              </div>
-            );
-          }
+        // if (item.type === 'dynamic_blog' && item.blog_tag) {
+        //   if (Object.keys(recentBlogPosts).length === 0) {
+        //     // If recentBlogPosts.length === 0, then either there is no .env.development,
+        //     // it has a bad blog url, or the endpoint returned something bad
+        //     return (
+        //       <div className="contextual-links__alert" role="alert">
+        //         <p>
+        //           You are currently in develop mode.
+        //           Dynamic blog posts will not be displayed locally.
+        //           <a style={{ fontSize: 'inherit' }} href="https://github.com/postmanlabs/postman-docs/blob/develop/CONTRIBUTING.md#to-use-the-dynamic-blog-posts-feature" target="_blank" rel="noopener">See Contributing doc for details</a>
+        //           .
+        //         </p>
+        //       </div>
+        //     );
+        //   }
           if (recentBlogPosts.index && recentBlogPosts.index[item.blog_tag]) {
             const dynamicLinks = recentBlogPosts.index[item.blog_tag].slice(0, 3).map((post) => (
               <div className="contextual-links__link" key={uuidv4()}>
@@ -103,8 +103,8 @@ const ContextualLinks = ({ links }) => (
               </div>
             ));
             return (
-              <div  key={uuidv4()}>
-                <div className="contextual-links__subtitle" >
+              <div key={uuidv4()}>
+                <div className="contextual-links__subtitle" key={item.name}>
                   {item.name}
                 </div>
                 {dynamicLinks}
@@ -117,8 +117,8 @@ const ContextualLinks = ({ links }) => (
 
           // This will likely be the case when you are developing locally,
           // and do not have a .env.development file with the BFF endpoints.
-          return null;
-        }
+        //   return null;
+        // }
 
         return (
           <div className="contextual-links__section" key={uuidv4()}>{item.name}</div>
