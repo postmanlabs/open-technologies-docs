55dceb69c5e781318e3f42246b19d32ca4eac0a3
OlliesWorld <rtlockwoodwork@gmail.com>
2023-01-27 15:33:38 -0700

    filter on cards, not working rebase

---
+++ src/components/filterCards/FilterCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ConceptCard } from "../MarketingPages/ConceptCards";
+// const { v4: uuidv4 } = require('uuid');
+
+
+
+
+const FilterCard = ({ item }) => {
+  // console.log(item, "card")
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          {item.map((info) => {
+            return (
+              <div key={info.id} className="col-lg-6 mb-3 mb-md-4 ">
+
+              <ConceptCard
+                title={info.title}
+                description={info.description}
+                tag={info.tag}
+                code={info.code}
+              />
+            </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FilterCard;
