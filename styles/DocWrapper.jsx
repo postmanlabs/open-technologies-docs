5594303d7d8c0f84d217b092fed929f586a86781
OlliesWorld <rtlockwoodwork@gmail.com>
2023-02-01 14:41:06 -0700

    add hub page

--- styles/DocWrapper.jsx
+++ styles/DocWrapper.jsx
@@ -1,21 +1,8 @@
-/* eslint-disable react/no-danger */
-import React from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/layout';
-import RightNavLinks from '../components/RightNavLinks';
-import { leftNavItems } from '../components/LeftNav/LeftNavItems';
-import LeftNav from '../components/LeftNav/LeftNav';
-import SEO from '../components/seo';
+import { BaseLinkStyles } from 'aether-marketing';
 import styled from 'styled-components';
 import 'prismjs/themes/prism-tomorrow.css';
-import BreadCrumbsLinks from '../components/modules/BreadCrumbsLinks';
-import PreviousAndNextLinks from '../components/modules/PreviousAndNextLinks';
-import { BaseLinkStyles } from 'aether-marketing';
-// import Data from '../components/filterCards/Data';
-// import { ConceptCard } from '../components/MarketingPages/ConceptCards';
-import ConceptCardFilter from '../components/filterCards/ConceptCardFilter';
 
-const DocWrapper = styled.div`
+export const DocWrapper = styled.div`
   /* Used for Deeplinking */   
 h2, h3, h4 {
   scroll-margin-top: 2em;
@@ -251,98 +238,3 @@ code[class*="language-"] {
     color: ${(props) => props.theme.colors.blue_80};
 }
 `
-
-const RightColumnWrapper = styled.aside`
-  margin-top: 0px;
-  padding-left: 40px !important;
-
-  @media (min-width:992px) {
-    margin-top: 119px;
-    padding-right: 24px;
-    padding-left: 0px !important;
-  }
-
-.sticky {
-  margin-top: 64px;
-  position: -webkit-sticky;
-  position: sticky !important;
-  top: 160px;
-}
-
-.postmanaut-dab {
-  max-width: 250px;
-}
-.dynamic-link  {
-  width: 100;
-}
-`
- 
-const GraphQLPage = ({data}) => {
- 
-  const { parentLink, subParentLink, previous, next } = data;
-
-  return (
-    <Layout>
-      <SEO title="Postman GraphQL"
-          description='GraphQL activities deliver both internal and external value for Postman.'
-          slug="/graphql/" />
-      <DocWrapper className="container-fluid">
-        <div className="row row-eq-height">
-          <nav className="col-md-4 col-lg-3 left-nav-re">
-            <LeftNav leftNavItems={leftNavItems} />
-          </nav>
-          <div className="col">
-            <div className="row row-eq-height">
-              <main className="col-lg-9 offset-lg-0 col-xl-7 doc-page ml-xl-5">
-              <BreadCrumbsLinks data={{ parentLink, subParentLink }} />
-                {/* Qualtrics */}
-
-              <h2 className="mt-5 mb-3">GraphQL Concepts</h2>
-              <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem numquam vitae ratione eveniet fuga assumenda id dolor corrupti. Aperiam ab a reiciendis, odit dignissimos tempore minima autem cum animi pariatur!
-              </p>
-          
-              <div className="row justify-content-evenly">           
-                <div className="container-fluid">
-                      <div className="row">
-                        {/* {Data.map((item) => (
-                          <div className="col-lg-6 mb-3 mb-md-4"  key={item.id}>    
-                            <ConceptCard title={item.title}
-                                  description={item.description}
-                                  tag={item.tag}
-                                  code={item.code} />
-                          </div>
-                          ))} */}
-                          <ConceptCardFilter />
-                      </div>
-                </div>     
-              </div>             
-            <PreviousAndNextLinks data={{ previous, next }} />
-          </main>
-              <RightColumnWrapper className="col-lg-2 offset-lg-0 col-xl-3 offset-xl-1 right-column">
-                          
-                <RightNavLinks />
-                 
-              </RightColumnWrapper>
-            </div>
-          </div>
-        </div>
-      </DocWrapper>
-    </Layout>
-  )
-}
-export const query = graphql`
-  query {
-    allMarkdownRemark {
-    nodes {
-      fields {
-        slug
-        lastModifiedDate
-        lastModifiedTime
-      }
-    }
-  }
-  }
-`;
-export default GraphQLPage;
-/* eslint-enable */
