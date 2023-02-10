14f17863b1cb7d5d2f934c2a18378620628acc96
Brandon Castillo <bcastillo.dev@gmail.com>
2023-02-10 14:22:06 -0800

    Update filter for concept cards

--- src/components/filterCards/FilterButtons.jsx
+++ src/components/filterCards/FilterButtons.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { ButtonTag } from "./Tag";
 const { v4: uuidv4 } = require('uuid');
 
-const FilterButtons = ({ button, filter }) => { 
-  
+const FilterButtons = ({ button, filter, activeButton }) => { 
   return (
     <>
       <div className="col-md-12 justify-content-center mx-auto mb-5">
       {
-        button.sort().map((tag, i)=>{
-          return <ButtonTag key={uuidv4()} data-text={tag} type="button" onClick={()=> filter(tag)} className="btn">{tag}</ButtonTag>
+          button.sort().map((tag, i) => {
+            return <ButtonTag key={uuidv4()} data-text={tag} type="button" onClick={() => filter(tag)} className={activeButton === tag ? 'btn active' : 'btn'}>{tag}</ButtonTag>
               })
             }
       </div>
