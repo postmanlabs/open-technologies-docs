3af90f9f1053d104a3ec58e71611bd42bdb35cce
Michael Claus <michael.claus@postman.com>
2023-02-16 16:25:27 -0800

    Add back assetPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,8 @@ module.exports = {
     author: 'Postman',
     siteUrl: siteUrl,
     pathPrefix: PATH_PREFIX
-  }, 
+  },
+  assetPrefix: PATH_PREFIX, // The assetPrefix key is apart of gatsby build process, can find in gatsby docs
   pathPrefix: PATH_PREFIX, // The pathPrefix key is apart of gatsby build process, can find in gatsby docs
   trailingSlash: 'always',
   plugins: [
