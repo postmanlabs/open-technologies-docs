a0c284597b7b0bd8196fe74c4228c4d50c351cf7
OlliesWorld <rtlockwoodwork@gmail.com>
2023-02-22 09:00:15 -0700

    add dates to calendar events

--- src/components/calendarDates.jsx
+++ src/components/calendarDates.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql, useStaticQuery } from "gatsby";
 const { v4: uuidv4 } = require('uuid');
+import { format } from 'date-fns';
 
 function CalendarDates() {  
  
@@ -9,7 +10,7 @@ function CalendarDates() {
         calendarData {
           records {
             fields {
-            date: Start_Time
+            date: Start_Time(formatString: "MM-DD-YYYY")
             title: Title
             duration: Duration
             Link
@@ -26,6 +27,7 @@ return (
   <div >         
     {Events.map((event) => (
       <div className='mb-2' key={uuidv4()}>
+        <p className='mb-0 '>{event.fields.date}</p>
         { event.fields.Link ? (<a  href={event.fields.Link} target="_blank" rel="noopener">{event.fields.title} →</a>)  : event.fields.title }
       </div>  
       ))}
