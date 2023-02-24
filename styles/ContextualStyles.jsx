37622f4f46584154f636e40118ddddbb87f8bbdf
OlliesWorld <rtlockwoodwork@gmail.com>
2023-02-24 10:57:11 -0700

    move styles to own file, fix keyboard pagination

---
+++ styles/ContextualStyles.jsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import { theme } from "./theme";
+
+export const ContextualStyles = styled.div`
+
+   .contextual-links__section {
+    margin-top: 24px;
+    margin-bottom: 8px;
+    font-size: 16px;
+    font-weight: 600;
+    color: ${(props) => props.theme.colors.grey_80};
+  }
+  .contextual-links__link {
+    margin-bottom: 0;
+    padding: 4px 0;
+    display: block;
+    
+    a {
+      color: ${(props) => props.theme.colors.grey_50};
+      font-size: 14px;
+      text-decoration: none;
+      border-bottom: 1px solid ${(props) => props.theme.colors.grey_00};
+      transition: ${(props) => props.theme.transitions.all};
+    }
+    
+    &:hover {
+      a {
+        color: ${(props) => props.theme.colors.blue_60};
+        border-bottom: 1px solid ${(props) => props.theme.colors.blue_60};
+        transition: ${(props) => props.theme.transitions.all};
+      }
+    }
+  }
+
+  .contextual-links__subtitle {
+    color: ${(props) => props.theme.colors.grey_60};
+    font-size: 15px;
+    font-weight: 600;
+    margin-top: 1rem;
+    margin-bottom: .7rem;
+  }
+
+  .contextual-links__alert {
+    border: 4px dashed ${(props) => props.theme.colors.grey_10};
+    border-radius: ${(props) => props.theme.borderRadius.small};
+    padding: .75rem 1.25rem;
+    color: #0C5460;
+  }
+
+  // tablet view
+  @media (min-width: 767px) and (max-width: 990px) {
+      padding-bottom: 80px;
+  }
+
+
+  // mobile view
+  @media (max-width: 767px) {
+    padding-bottom: 80px;
+  }
+
+
+`
