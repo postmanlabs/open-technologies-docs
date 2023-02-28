dd17a07d3a30c7d8003e439e445e157b4cbdfba2
OlliesWorld <rtlockwoodwork@gmail.com>
2023-02-28 14:20:16 -0700

    cleanup the handlePagination file

--- src/components/modules/handlePagination.jsx
+++ src/components/modules/handlePagination.jsx
@@ -1,20 +1,18 @@
 /* Use <- and -> keys to navigate between docs on web */
 export const handleKeyboard = () => {
-  // const togglePrevLink = document.querySelector('.prevDoc');
-  // const toggleNextLink = document.querySelector('.nextDoc');
 
   document.addEventListener('keydown', (e) => {
-    const togglePrevLink =  document.getElementById('prevDoc');
-    const toggleNextLink =  document.getElementById('nextDoc');
-   if(togglePrevLink != undefined){
-    if (e.key === 'ArrowLeft') {
-      togglePrevLink.click();
+    const togglePrevLink = document.querySelector('.prevDoc');
+    const toggleNextLink = document.querySelector('.nextDoc');
+    if (togglePrevLink != undefined) {
+      if (e.key === 'ArrowLeft') {
+        togglePrevLink.click();
+      }
     }
-  }
-  if(toggleNextLink != undefined){
-    if (e.key === 'ArrowRight') {
-      toggleNextLink.click();
+    if (toggleNextLink != undefined) {
+      if (e.key === 'ArrowRight') {
+        toggleNextLink.click();
+      }
     }
-  }
   })
 }
