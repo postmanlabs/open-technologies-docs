d4124b9c10df5619497d1975db712eca8b8c8324
OlliesWorld <rtlockwoodwork@gmail.com>
2023-03-14 08:20:32 -0600

    fix doc styles anchor link

--- src/templates/doc.jsx
+++ src/templates/doc.jsx
@@ -214,17 +214,17 @@ ol {
 a.anchor.before {
     top: 0.2em;
     padding-right: 8px;
-
+    svg {
       width: auto;
       height: 0.9em;
       display: block;
     }
   }
-
   a.anchor:hover {
     display: block;
     border: none !important;
   }
+
 `
 const prefixImgSrcOfParsedHtml = (parsedHtml, domainName) => {
   // This function prefixes all relative image srcs of the parsedHTML's image srcs with the pathPrefix defined in the gatsby-config.js file
