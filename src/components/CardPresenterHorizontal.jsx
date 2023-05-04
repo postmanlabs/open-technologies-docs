1e1d456f10087eb3a4a7c284220df8213e7ab933
OlliesWorld <rtlockwoodwork@gmail.com>
2023-05-04 15:50:23 -0600

    [212GitHub]: forgot to import octokit

--- src/components/CardPresenterHorizontal.jsx
+++ src/components/CardPresenterHorizontal.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Octokit } from "@octokit/rest";
 import { withPrefix } from 'gatsby';
 
 const SVG = styled.svg`
